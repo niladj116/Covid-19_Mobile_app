bf52512a651bcff0864dde1a3a1116951f17e7f6
Niladri Sadhukhan <niladri@niladris-mbp.lan>
2020-04-06 00:28:34 -0400

    Initial version Covid-19 app

---
+++ www/Key.js
@@ -0,0 +1,24 @@
+var KEY="AIzaSyAFO0y-gtnsuG1azfQxKTXE_2HpLJcrpW4";
+var URL_KEY="1dbdf75773msh5d8ff64d5df24e3p1783d1jsn46f186225dfe";
+//var KEY1='AIzaSyCzWvj-vD2mlxn8HL9YJHXJehxgd5n0Ync';
+var KEY1='AIzaSyCWaOrcRTbX5uPrt6x8f6o7v0UGF2RZT0k';
+
+//function GoogleChart(){
+//     
+//    this.initialize = function(){
+//        var map = showChart();
+//    }
+//     
+//    var showChart = function(){
+//        var mapOptions = {
+//        zoom: 4,
+//        center: new google.maps.LatLng(-33, 151),
+//        mapTypeId: google.maps.MapTypeId.ROADMAP
+//        }
+//
+////        var chart = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+//        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+//
+//        return map;
+//    }
+//}
